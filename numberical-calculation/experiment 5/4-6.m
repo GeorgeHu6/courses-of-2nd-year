59e9f8746eb9ad06ac1dac6f36e2e61c7064a8af
GeorgeHu <dhxxhch@126.com>
2021-11-06 11:24:10 +0800

    complete data structure exp6

--- numberical-calculation/experiment 5/4-6.m
+++ numberical-calculation/experiment 5/4-6.m
@@ -1,8 +1,7 @@
-x0 = 0:50:29000;
-y0 = 0:50:19000;
+[x0,y0] = meshgrid(0:50:29000,0:50:19000);
 a = load('data1.txt');
-x = a(:,1);
-y = a(:,2);
-z = a(:,3);
-[X,Y,Z] = griddata(x,y,z,x0,y0,'cubic');
+x = a(:,1)';
+y = a(:,2)';
+z = a(:,3)';
+[X,Y,Z] = griddata(x,y,z,x0,y0,'linear');
 mesh(X,Y,Z)
